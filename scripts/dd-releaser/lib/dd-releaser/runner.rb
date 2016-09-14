eb6379c1060fcbf876c9c278b95ee4d86447b48d
Denis Defreyne <denis.defreyne@stoneship.org>
2016-09-14 21:45:31 +0200

    Read and validate release notes

--- scripts/dd-releaser/lib/dd-releaser/runner.rb
+++ scripts/dd-releaser/lib/dd-releaser/runner.rb
@@ -9,28 +9,48 @@ module DDReleaser
 
     def run
       version = read_version
-      validate_release_notes
+      release_notes = read_release_notes
+
+      validate(version, release_notes)
     end
 
     private
 
-    def read_version
-      @io.puts '=== Reading version…'
-      load(@version_file)
-      version = eval(@version_constant)
-      @io.puts "Version = #{version}"
-      @io.puts
+    ReleaseNotes = Struct.new(:version, :date, :body)
 
-      version
+    def read_version
+      load(@version_file)
+      eval(@version_constant)
     end
 
-    def validate_release_notes
-      puts '=== Validating release notes…'
-      unless File.readlines('NEWS.md').drop(2).first =~ / \(\d{4}-\d{2}-\d{2}\)$/
-        $stderr.puts 'No proper release date found!'
-        exit 1
+    def read_release_notes
+      lines = File.readlines('NEWS.md')
+
+      identifier_line = lines.drop(2).first
+      body = lines.drop(4).take_while { |l| l !~ /^## / }.join
+
+      regexp = /^## (\d\.\d\.\d) \((\d{4}-\d{2}-\d{2})\)$\n/
+      version = identifier_line.sub(regexp, '\1')
+      date = identifier_line.sub(regexp, '\2')
+
+      ReleaseNotes.new(version, date, body)
+    end
+
+    def validate(version, release_notes)
+      unless release_notes.version =~ /\A\d\.\d\.\d\z/
+        @io.puts "ERROR: version specified in release notes is malformed"
+        exit
       end
-      puts
+
+      unless release_notes.date =~ /\A\d{4}-\d{2}-\d{2}\z/
+        @io.puts "ERROR: date specified in release notes is malformed"
+        exit
+      end
+
+      unless release_notes.version == version
+        @io.puts "ERROR: version specified in release notes is incorrect"
+        exit
+      end
     end
   end
 end
